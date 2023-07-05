d67de64c95c493731dcdfea76f3577dfcdb10291
JeonSuHyeong <64085393+JeonSuHyeong@users.noreply.github.com>
2023-07-05 16:22:25 +0900

    Update SASceneNet_train.py

--- Scene-Recognition/jsh/SASceneNet_train.py
+++ Scene-Recognition/jsh/SASceneNet_train.py
@@ -17,29 +17,13 @@ from Libs.Datasets.MITIndoor67Dataset_gray import MITIndoor67Dataset
 from Libs.Utils import utils
 from Libs.Utils.torchsummary import torchsummary
 import numpy as np
+import yaml
 from tqdm import tqdm
 import random
 from mit_semseg.models import ModelBuilder, SegmentationModule
 import cv2
-import yaml
-os.environ["CUDA_VISIBLE_DEVICE"]="1"
 
-device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-
-#이것도 모델안에 넣어두는게 best일듯
-net_encoder = ModelBuilder.build_encoder(
-    arch="resnet50",
-    fc_dim=2048,
-    weights="ckpt/ade20k-resnet50-upernet/encoder_epoch_30.pth")
-net_decoder = ModelBuilder.build_decoder(
-    arch="upernet",
-    fc_dim=2048,
-    num_class=150,
-    weights="ckpt/ade20k-resnet50-upernet/decoder_epoch_30.pth",
-    use_softmax=True)
-crit = nn.NLLLoss(ignore_index=-1)
-
-model2 = SegmentationModule(net_encoder, net_decoder, crit)
+global USE_CUDA, classes, CONFIG
 
 def seed_everything(seed):
     random.seed(seed)
@@ -65,6 +49,9 @@ def validation(dataloader, model, set):
     Predictions = np.zeros(len(dataloader))
     SceneGTLabels = np.zeros(len(dataloader))
 
+    model.eval()
+    model2.eval()
+
     # Extract batch size
     batch_size = CONFIG['VALIDATION']['BATCH_SIZE']['VAL']
 
@@ -78,19 +65,11 @@ def validation(dataloader, model, set):
                 RGB_image = mini_batch['Image'].cuda()
                 RGB_images_np = RGB_image.cpu().numpy().transpose(0, 2, 3, 1)
                 gray_images_np = rgb_to_gray(RGB_images_np)
-                #gray_images_np shape (10,224,224)
                 gray_images_tensor = torch.tensor(gray_images_np).unsqueeze(1).cuda()
-                
                 semantic_scores = model2(RGB_image, feed_dict=None, segSize=(224,224))
-                #print(semantic_scores.shape) [10,150,224,224]
-                #여기서 10,3,224,224가 되야한다.
-                # 아마 ade20k 기준으로 한다면 10, 150 ,224,224가 나올듯
-                # model output 값을 ordered dict에서 tensor로 변환도 필요. -> key 'out'일때 output tensor
                 sceneLabelGT = mini_batch['Scene Index'].cuda()
-                gray_segmentation_tensor=process_segmentation_output(semantic_scores, gray_images_tensor, threshold=0.5, batch_size=CONFIG['TRAINING']['BATCH_SIZE']['TRAIN'])
-                
-                sceneLabelGT = mini_batch['Scene Index'].cuda()
-
+                gray_segmentation_tensor=process_segmentation_output(semantic_scores, gray_images_tensor, batch_size=len(gray_images_np), threshold=0.5)
+            
             # Model Forward
             outputSceneLabel, _, _, _ = model(RGB_image, gray_segmentation_tensor)
             # Compute class accuracy
@@ -141,7 +120,7 @@ def rgb_to_gray(images):
         gray_images.append(gray_image)
     return np.stack(gray_images)
 
-def process_segmentation_output(segmentation_output, gray_images, threshold=0.5, batch_size=10):
+def process_segmentation_output(segmentation_output, gray_images, threshold=0.5, batch_size):
     # 확률 임계값보다 높은 부분만 남기고, 나머지는 0으로 설정
     thresholded_output = torch.where(segmentation_output > threshold, segmentation_output, torch.tensor(0.0).cuda())
 
@@ -153,7 +132,20 @@ def process_segmentation_output(segmentation_output, gray_images, threshold=0.5,
     final_output = gray_images_with_channel * thresholded_output
 
     return final_output
+"""
+def process_segmentation_output_test(segmentation_output, gray_images, batch_size,threshold=0.5):
+    # 확률 임계값보다 높은 부분만 남기고, 나머지는 0으로 설정
+    thresholded_output = torch.where(segmentation_output > threshold, segmentation_output, torch.tensor(0.0).cuda())
 
+    # 흑백 이미지에 채널 차원 추가 (10, 1, 224, 224)
+    # segmentation_output의 차원에 맞게 gray_images_with_channel 차원 조정
+    gray_images_with_channel = gray_images.expand(batch_size, 151, 224, 224)
+
+    # 흑백 이미지와 임계값이 적용된 segmentation 출력을 곱함
+    final_output = gray_images_with_channel * thresholded_output
+
+    return final_output
+"""
 def train(dataloader, model, optimizer, scheduler= None, device = 'cuda'):
     # Start data time
     best_val_acc = 0
@@ -162,6 +154,7 @@ def train(dataloader, model, optimizer, scheduler= None, device = 'cuda'):
     best_model = None
     model.to(device)
     model2.to(device)
+    model2.eval()
     patient = 0
     for epoch in range(1, CONFIG['TRAINING']['EPOCHS']+1):
         model.train()
@@ -170,38 +163,29 @@ def train(dataloader, model, optimizer, scheduler= None, device = 'cuda'):
         for i, (mini_batch) in tqdm(enumerate(dataloader)):
             if USE_CUDA:
                 RGB_image = mini_batch['Image'].cuda()
+                sceneLabelGT = mini_batch['Scene Index'].cuda()
+
                 RGB_images_np = RGB_image.cpu().numpy().transpose(0, 2, 3, 1)
                 gray_images_np = rgb_to_gray(RGB_images_np)
-                #gray_images_np shape (10,224,224)
                 gray_images_tensor = torch.tensor(gray_images_np).unsqueeze(1).cuda()
                 
                 semantic_scores = model2(RGB_image, feed_dict=None, segSize=(224,224))
-                #print(semantic_scores.shape) [10,150,224,224]
-                #여기서 10,3,224,224가 되야한다.
-                # 아마 ade20k 기준으로 한다면 10, 150 ,224,224가 나올듯
-                # model output 값을 ordered dict에서 tensor로 변환도 필요. -> key 'out'일때 output tensor
-                sceneLabelGT = mini_batch['Scene Index'].cuda()
-                gray_segmentation_tensor=process_segmentation_output(semantic_scores, gray_images_tensor, threshold=0.5)
+                gray_segmentation_tensor=process_segmentation_output(semantic_scores, gray_images_tensor, batch_size=len(gray_images_np), threshold=0.5)
 
             #TODO: make_one_hot 수정
-            #semanticTensor = utils.make_one_hot(semantic_mask, semantic_scores, C=CONFIG['DATASET']['N_CLASSES_SEM'])
-            #print(semanticTensor.shape)
-            #10,152,224,224 shape은 같다. 이제 make_one_hot 분석후, 비슷하게 바꿔야한다.
             optimizer.zero_grad()
 
-            #outputSceneLabel, _, _, _= model(RGB_image, semanticTensor)
             outputSceneLabel, _, _, _ = model(RGB_image, gray_segmentation_tensor)
             
-
             loss = model.loss(outputSceneLabel, sceneLabelGT)
 
             loss.backward()
-
             optimizer.step()
+
             train_loss.append(loss.item())
 
             #validation: _val_top1, val_loss만 사용하기
-        _val_top1, val_top2, val_top5, _val_loss, val_ClassTPDic = validation(val_loader, model, set='Validation')
+        _val_top1, _, _, _val_loss, _ = validation(val_loader, model, set='Validation')
         _train_loss = np.mean(train_loss)
 
         print(f'Epoch [{epoch}], Train Loss : [{_train_loss:.5f}] Val Loss : [{_val_loss:.5f}] Val ACC : [{_val_top1:.5f}]')
@@ -210,20 +194,18 @@ def train(dataloader, model, optimizer, scheduler= None, device = 'cuda'):
 
         if scheduler is not None:
             scheduler.step(_val_top1)
-        if _val_loss < best_val_loss:
-            if best_val_acc <= _val_top1:
-                best_val_acc = _val_top1
-                best_val_loss = _val_loss
-                best_model = {
-                    'state_dict': model.state_dict(),
-                    'arch': model.__class__.__name__,
-                    'optimizer_state_dict': optimizer.state_dict(),
-                    'epoch': epoch
-                }
-                torch.save(best_model, f'best_model_gray_epoch_{epoch}.pth')
-                patient = 0
-            else:
-                patient+=1
+
+        if best_val_acc < _val_top1:
+            best_val_acc = _val_top1
+            best_val_loss = _val_loss
+            best_model = {
+                'state_dict': model.state_dict(),
+                'arch': model.__class__.__name__,
+                'optimizer_state_dict': optimizer.state_dict(),
+                'epoch': epoch
+            }
+            torch.save(best_model, f'best_model_test_epoch_{epoch}.pth')
+            patient = 0
         else:
             patient+=1
         #if patient == 20:
@@ -231,14 +213,10 @@ def train(dataloader, model, optimizer, scheduler= None, device = 'cuda'):
 
     return best_model
 
-global USE_CUDA, classes, CONFIG
+os.environ["CUDA_VISIBLE_DEVICE"]="1"
 
 # Decode CONFIG file information
 CONFIG = yaml.safe_load(open("Config/config_MITIndoor_365_finetuning.yaml", 'r'))
-USE_CUDA = torch.cuda.is_available()
-
-PATH = CONFIG['MODEL']['PATH'] + 'TRAINSAVE/'
-
 print('-' * 65)
 print("Evaluation starting...")
 print('-' * 65)
@@ -248,7 +226,6 @@ print('Selected RG backbone architecture: ' + CONFIG['MODEL']['ARCH'])
 model = SASceneNet(arch=CONFIG['MODEL']['ARCH'], scene_classes=CONFIG['DATASET']['N_CLASSES_SCENE'], semantic_classes=CONFIG['DATASET']['N_CLASSES_SEM'])
 
 # Load the trained model + use place365 and finetuning
-#completePath = CONFIG['MODEL']['PATH'] + 'TRAINSAVE/' +'best_model_epoch.pth'
 completePath = CONFIG['MODEL']['PATH'] + 'SAScene_ResNet18_Places.pth.tar'
 
 if os.path.isfile(completePath):
@@ -256,14 +233,13 @@ if os.path.isfile(completePath):
     checkpoint = torch.load(completePath)
     best_prec1 = checkpoint['best_prec1']
     model.load_state_dict(checkpoint['state_dict'])
-    #TODO : freeze 필요
     print("Loaded model {} from path {}.".format(CONFIG['MODEL']['NAME'], completePath))
     print("     Epochs {}".format(checkpoint['epoch']))
     print("     Single crop reported precision {}".format(best_prec1))
 else:
     print("No checkpoint found at '{}'. Check configuration file MODEL field".format(completePath))
     quit()
-    
+
 model.in_block_sem = nn.Sequential(
             nn.Conv2d(150, 64, kernel_size=7, stride=2, padding=3, bias=False),
             nn.BatchNorm2d(64),
@@ -271,17 +247,34 @@ model.in_block_sem = nn.Sequential(
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
         )
 
-
 #for number of class change
 num_features = model.fc.in_features
 model.fc = nn.Linear(num_features,6)
-
 num_features_SEM = model.fc_SEM.in_features
 model.fc_SEM = nn.Linear(num_features_SEM,6)
-
 num_features_RGB = model.fc_RGB.in_features
 model.fc_RGB = nn.Linear(num_features_RGB,6)
 
+
+
+
+USE_CUDA = torch.cuda.is_available()
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+net_encoder = ModelBuilder.build_encoder(
+    arch="resnet50",
+    fc_dim=2048,
+    weights="ckpt/ade20k-resnet50-upernet/encoder_epoch_30.pth")
+net_decoder = ModelBuilder.build_decoder(
+    arch="upernet",
+    fc_dim=2048,
+    num_class=150,
+    weights="ckpt/ade20k-resnet50-upernet/decoder_epoch_30.pth",
+    use_softmax=True)
+crit = nn.NLLLoss(ignore_index=-1)
+
+model2 = SegmentationModule(net_encoder, net_decoder, crit)
 
 # Move Model to GPU an set it to evaluation mode
 if USE_CUDA:
@@ -375,8 +368,4 @@ print('Evaluation statistics:')
 print('Validation results: Loss {val_loss:.3f}, Prec@1 {top1:.3f}, Prec@2 {top2:.3f}, Prec@5 {top5:.3f}, '
       'Mean Class Accuracy {MCA:.3f}'.format(val_loss=val_loss, top1=val_top1, top2=val_top2, top5=val_top5,
                                               MCA=np.mean(val_ClassAcc_top1)))
-
-print("Save Model")
-torch.save(model.state_dict(), PATH + 'btest_model.pt')
-
 print("complete")
